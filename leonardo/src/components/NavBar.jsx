24d2ed12fa3c4eb28e77078bbf70eabcd8170e5f
Leonardo Guerrero <leo.19-20@hotmail.com>
2024-04-24 23:45:32 -0500

    navbar

--- leonardo/src/components/NavBar.jsx
+++ leonardo/src/components/NavBar.jsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 
+
 export const NavBar = () => {
 
     return (
         <>
-            <div>
-                <h1>
-                    <Link>Leonardo Guerrero</Link>
+            <div className="bg-sky-900 text-white flex justify-between py-5 px-20">
+                <h1 className="font-bold text-xl">
+                    <Link href="/">Leonardo Guerrero</Link>
                 </h1>
 
                 <ul>
