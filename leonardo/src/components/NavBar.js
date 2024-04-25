5f9d435218025fb30252e4e74eaec3627a5b1b4a
Leonardo Guerrero <leo.19-20@hotmail.com>
2024-04-24 23:31:16 -0500

    cambios

---
+++ leonardo/src/components/NavBar.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export const NavBar = () => {
+
+    return (
+        <>
+            <div>
+                <h1>
+                    <Link>Leonardo Guerrero</Link>
+                </h1>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </>
+    )
+
+};
